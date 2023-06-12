955a9fcd78448969976a56d7d2dd699004e13f53
igorwab <131726359+igorwab@users.noreply.github.com>
2023-06-12 20:23:03 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// script.js
+
+document.addEventListener('copy', function(event) {
+    event.preventDefault(); // Impede a ação de copiar
+    alert('Conteúdo bloqueado'); // Exibe o popup com a mensagem de aviso
+  });
+  
